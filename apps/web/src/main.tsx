ac583f94e2e9e2412b26cd7f2a7aa6a97ce323bc
Murilo Neder <murilo.csn@gmail.com>
2026-08-28 11:06:30 -0300

    Versão testada em area local, subindo para teste

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./styles.css";
@@ -19,7 +19,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter
+      <HashRouter
         future={{
           v7_relativeSplatPath: true,
           v7_startTransition: true,
@@ -28,7 +28,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <AuthProvider>
           <App />
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
